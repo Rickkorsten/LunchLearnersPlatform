73d91ef7e3a68e07f3a5637a31c09ed6fdeff55f
Tom Gielen <tomgielen007@gmail.com>
2018-02-23 11:58:11 +0100

    Material updated

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule, MatIconModule, MatGridListModule, MatCardModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatGridListModule, MatCardModule],
   exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatGridListModule, MatCardModule ],
 })
 export class MaterialModule { }
