511444fa962db05a837389846a0fb386da08eca0
Tom Gielen <tomgielen007@gmail.com>
2018-02-23 13:46:12 +0100

    Firestore moved from main component to store compontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-
-interface Post {
-  title: string;
-  content: string;
-}
 
 
 @Component({
@@ -16,22 +8,5 @@ interface Post {
 })
 export class AppComponent {
 
-  postsCol: AngularFirestoreCollection<Post>;
-  posts: Observable<Post[]>;
-
-  title:string;
-  content:string;
-
-  constructor(private afs: AngularFirestore) {
-    
-  }
-
-  ngOnInit() {
-    this.postsCol = this.afs.collection('posts');
-    this.posts = this.postsCol.valueChanges();
-  }
-
-  addPost() {
-    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
-  }
+  
 }
--- src/components/store/store.component.ts
+++ src/components/store/store.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.css']
-})
+}) 
+
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
 
-  ngOnInit() {
+  title:string;
+  content:string;
+
+  constructor(private afs: AngularFirestore) {
+    
   }
 
+  ngOnInit() {
+    this.postsCol = this.afs.collection('posts');
+    this.posts = this.postsCol.valueChanges();
+  }
+
+  addPost() {
+    this.afs.collection('posts').add({'title': this.title, 'content': this.content});
+  }
 }
