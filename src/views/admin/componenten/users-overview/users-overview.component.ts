3a9e48793e0ebcee3fe7f91146f4a00922b55206
rick <rickkorsten@live.nl>
2018-03-08 12:34:14 +0100

    added the fab button to the user overview page

--- src/views/admin/componenten/users-overview/users-overview.component.ts
+++ src/views/admin/componenten/users-overview/users-overview.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { PostBookComponent } from './../../../../components/post-book/post-book.component';
+import { CompanyDialogComponent } from './../../../../components/company-dialog/company-dialog.component';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AuthService } from '../../../../app/core/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
 
 @Component({
   selector: 'app-users-overview',
@@ -7,7 +15,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersOverviewComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+
+  constructor(private db: AngularFirestore, public dialog: MatDialog, private storage: AngularFireStorage, public auth: AuthService) {
+    this.user == auth.user;
+    console.log(this.user);
+  }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(CompanyDialogComponent, {
+      width: '300px',
+      data: { }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
 
   ngOnInit() {
   }
