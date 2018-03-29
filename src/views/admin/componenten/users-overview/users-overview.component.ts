9ee4d515c20897eae66f9bdb19f30aebaebc0226
rick <rickkorsten@live.nl>
2018-03-29 17:36:59 +0200

    created function that if delete user it also deletes user from company user array

--- src/views/admin/componenten/users-overview/users-overview.component.ts
+++ src/views/admin/componenten/users-overview/users-overview.component.ts
@@ -1,21 +1,22 @@
-import { AngularFireAuth } from 'angularfire2/auth';
+
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
-// import { AuthService } from '../../../../app/core/auth.service';
 import { MatDialog } from '@angular/material';
-// import * as admin from 'firebase-admin';
+
 
 interface User {
   displayName: string;
   email: string;
   photoURL: string;
   uid: string;
+  companyUid: string;
 }
 
 interface Company {
   name: string;
   code: string;
+  users: string[];
 }
 
 @Component({
@@ -27,14 +28,20 @@ export class UsersOverviewComponent implements OnInit {
 
   usersCol: AngularFirestoreCollection<User>;
   users: Observable<User[]>;
-
+  /// companies for dropdown
   companiesCol: AngularFirestoreCollection<Company>;
   companies: Observable<Company[]>;
+  // company for delete user
+  companyDelCol: AngularFirestoreCollection<Company>;
+  companyDel: Observable<Company[]>;
 
   selectedValue: string;
   all: string;
 
-  constructor(private db: AngularFirestore, public dialog: MatDialog, public auth: AngularFireAuth) {
+  usersArray: string[];
+  newArray: string[];
+
+  constructor(private db: AngularFirestore, public dialog: MatDialog) {
     this.all = 'All';
   }
 
@@ -46,12 +53,37 @@ export class UsersOverviewComponent implements OnInit {
     this.updateUserList('All');
   }
 
-  delete(uid) {
-    this.db.doc(`users/${uid}`).delete();
+  delete(uid, companyUid) {
+    // delete user from users collection
+    this.db.doc(`users/${uid}`).delete().then(
+    void this.getUsersArrayAndUpdate(uid, companyUid)
+     );
+  }
+
+  getUsersArrayAndUpdate(uid, companyUid) {
+    this.companyDelCol = this.db.collection('companies', ref => ref.where('uid', '==', companyUid));
+    this.companyDel = this.companyDelCol.valueChanges();
+    this.companyDel.subscribe(data => {
+      if (data[0].users) {
+        console.log(data[0].users);
+        this.usersArray = data[0].users;
+        if (this.usersArray) {
+          this.deleteFromCompanyUserlist(uid, companyUid, this.usersArray);
+        }
+      }
+
+    });
+  }
+
+  deleteFromCompanyUserlist(uid, companyUid, usersArray) {
+    // filter uid out of array
+    this.newArray = usersArray.filter(e => e !== uid);
+    this.db.doc(`companies/${companyUid}`).set({
+      'users': this.newArray
+    }, { merge: true });
   }
 
   updateUserList(selected) {
-    console.log(selected);
     if (selected === 'All') {
       this.usersCol = this.db.collection('users', ref => ref.where('role', '==', 'user'));
     } else {
