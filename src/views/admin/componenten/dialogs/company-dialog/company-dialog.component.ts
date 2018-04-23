001c0a83c39d99be79472bd19e5b38ae9cce20cb
rick <rickkorsten@live.nl>
2018-04-23 20:31:55 +0200

    updated company page in admin

--- src/views/admin/componenten/dialogs/company-dialog/company-dialog.component.ts
+++ src/views/admin/componenten/dialogs/company-dialog/company-dialog.component.ts
@@ -8,10 +8,36 @@ import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 })
 export class CompanyDialogComponent {
 
+  allBranches: string[];
+
   constructor(
 
     public dialogRef: MatDialogRef<CompanyDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+      this.allBranches = [
+        'Bouw en Vastgoed',
+        'Communicatie en Media',
+        'Consultancy',
+        'Energiebedrijven',
+        'Facilitaire dienstverlening',
+        'Fast Moving Consumer Goods',
+        'Financiële dienstverlening',
+        'Financiële instellingen',
+        'Gezondheidszorg en welzijnszorg',
+        'Handel en retail',
+        'Horeca, Recreatie, Toerisme en Cultuur',
+        'Industrie',
+        'Informatie en Communicatie Technologie (ICT)',
+        'Intermediairs',
+        'Juridische dienstverlening',
+        'Land- en tuinbouw',
+        'Onderwijs en Onderzoek',
+        'Overheid en semi-overheid',
+        'Technische dienstverlening',
+        'Telecommunicatie',
+        'Transport en Logistiek'
+      ];
+    }
 
   onNoClick(): void {
     this.dialogRef.close();
