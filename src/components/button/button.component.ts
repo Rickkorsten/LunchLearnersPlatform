47006c2ac3420b96dfa31dd592d8f35a4ac3d265
aylinnederlof <a.nederlof@student.fontys.nl>
2018-05-14 11:33:20 +0200

    merge

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -34,19 +34,16 @@ export class ButtonComponent implements OnInit {
         this.border = '#73C884';
         this.iconText = this.filled ? '#fff' : '#73C884';
         break;
-<<<<<<< HEAD
       case 'start':
        this.background = '#00000000';
        this.border = '#73C884';
        this.iconText = '#FFFFFF';
-=======
       case 'decline':
         this.background = this.filled ? '#707070' : '#fff';
         this.border = '#707070';
         this.iconText = this.filled ? '#fff' : '#707070';
         this.width = '269.6px';
         break;
->>>>>>> stylingLogin
     }
 
   }
