a26308d6a51fff345db3abd38cdd8ed0f4c8778a
mickkkk <m.vranken@student.fontys.nl>
2018-05-15 17:16:33 +0200

    added 'Uw boeken' to settingspage

---
+++ src/components/book-card-mini/book-card-mini.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, } from '@angular/core';
+
+@Component({
+  selector: 'app-book-card-mini',
+  templateUrl: './book-card-mini.component.html',
+  styleUrls: ['./book-card-mini.component.scss']
+})
+
+
+export class BookCardMiniComponent implements OnInit {
+
+  @Input() book: string;
+  @Input() title: string;
+  @Input() error: string;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
