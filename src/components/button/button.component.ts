db433f54ab3c6208dd6501bdc6a5f287898f375c
rick <rickkorsten@live.nl>
2018-05-15 10:19:56 +0200

    created banner

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -25,12 +25,12 @@ export class ButtonComponent implements OnInit {
 
     switch (this.type) {
       case 'card':
-        this.background = '#fff';
+        this.background = '#ffffff00';
         this.border = '#fff';
         this.iconText = '#3EA4D0';
         break;
       case 'accept':
-        this.background = this.filled ? '#73C884' : '#fff';
+        this.background = this.filled ? '#73C884' : '#ffffff00';
         this.border = '#73C884';
         this.iconText = this.filled ? '#fff' : '#73C884';
         break;
@@ -38,8 +38,9 @@ export class ButtonComponent implements OnInit {
        this.background = '#00000000';
        this.border = '#73C884';
        this.iconText = '#FFFFFF';
+       break;
       case 'decline':
-        this.background = this.filled ? '#707070' : '#fff';
+        this.background = this.filled ? '#707070' : '#ffffff00';
         this.border = '#707070';
         this.iconText = this.filled ? '#fff' : '#707070';
         this.width = '269.6px';
